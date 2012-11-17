9953c584fe261d7b3298be305fda4773a28b7d0c
root <root@master.(none)>
2012-11-17 15:31:08 +0000

    .gitingore is now working

--- RepastCommunicationDemo/processes/remote-distribution.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-# script to send to RepastProcess
-ant -f /root/gridspice.communicator/build.xml -Dlocal=$1 -Dtarget=$2 RemoteDistribution 
--- RepastCommunicationDemo/processes/remote-transmission.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-# script to receive messages from Repast Process
-ant -f /root/gridspice.communicator/build.xml -Dlocal=$1 -Dtarget=$2 RemoteTransmission
