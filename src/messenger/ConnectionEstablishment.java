7cde9c179d282e35c12c6bf414307fb7378d0be1
root <root@master.(none)>
2012-11-17 15:30:37 +0000

    New gitignore

--- src/messenger/ConnectionEstablishment.java
+++ src/messenger/ConnectionEstablishment.java
@@ -5,10 +5,9 @@ public class ConnectionEstablishment {
 		SYN, ACK, SYNACK
 	}
 
-        //Blahblah
 	public static final long DEFAULT_TIMEOUT = 50 * 1000; //in milliseconds
 	
 	public static final String REMOTE_DISTRIBUTION_SCRIPT = "remote-distribution.sh";
 	public static final String REMOTE_TRANSMISSION_SCRIPT = "remote-transmission.sh";
-	public static final String PATH_TO_SCRIPTS = "/Users/jimmydu/git/gridspice.communicator/RepastCommunicationDemo/processes/";
+	public static final String PATH_TO_SCRIPTS = "/root/gridspice.communicator/RepastCommunicationDemo/processes/";
 }
